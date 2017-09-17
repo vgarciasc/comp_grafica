2f18a44afb44bed26ea706726d00140faa442f29
vgarciasc <vingarciasc@gmail.com>
2017-09-17 19:09:38 -0300

    Messed with 'intersections.py'
    Using initial O(n²) algorithm to detect intersections.

--- exercicios/aula/interseccoes.py
+++ exercicios/aula/interseccoes.py
@@ -14,18 +14,27 @@ class Vector2:
     def __str__(self):
         return "(" + str(self.x) + ", " + str(self.y) + ")"
 
+class Line:
+    src = Vector2(0, 0)
+    dst = Vector2(0, 0)
+
+    def __init__(self, src, dst):
+        self.src = src
+        self.dst = dst
+
+    def __str__(self):
+        return "[" + str(self.src) + ", " + str(self.dst) + "]"
+
 createdLines = []
 
 def displayCallback():
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
     glLoadIdentity()
 
-    for pair in createdLines:
-        drawLine(
-            worldToScreenPoint(pair[0]), 
-            worldToScreenPoint(pair[1])
-        )
+    for line in createdLines:
+        drawLine(line)
 
+    renderIntersections()
     glutSwapBuffers()
 
 def mouseHandler(b, s, x, y):
@@ -44,7 +53,7 @@ def onMouseUp(x, y):
     addLine(mouseClickOrigin, mouseClickDestination)    
 
 def addLine(origin, destination):
-    createdLines.append([origin, destination])
+    createdLines.append(Line(origin, destination))
 
 def worldToScreenPoint(coord):
     width = glutGet(GLUT_WINDOW_WIDTH)
@@ -55,8 +64,62 @@ def worldToScreenPoint(coord):
         ((height / 2) - coord.y) / (height / 2)
     )
 
-def drawLine(origin, destination):
-    print(str(origin) + ", " + str(destination))
+def renderIntersections():
+    for i in range(0, len(createdLines)):
+        for j in range(i + 1, len(createdLines)):
+            line_1 = createdLines[i]
+            line_2 = createdLines[j]
+
+            drawIntersection(line_1, line_2)
+
+def drawIntersection(line_1, line_2):
+    a_1 = (line_1.dst.y - line_1.src.y) / (line_1.dst.x - line_1.src.x)
+    a_2 = (line_2.dst.y - line_2.src.y) / (line_2.dst.x - line_2.src.x)
+    b_1 = ((line_1.dst.x * line_1.src.y) - (line_1.src.x * line_1.dst.y)) / (line_1.dst.x - line_1.src.x)
+    b_2 = ((line_2.dst.x * line_2.src.y) - (line_2.src.x * line_2.dst.y)) / (line_2.dst.x - line_2.src.x)
+    
+    if a_1 == a_2:
+        print("Lines are parallel.")
+        return
+
+    x = (b_2 - b_1) / (a_1 - a_2)
+    y = a_2 * x + b_2
+
+    print ("line_1: " + str(line_1) + ", line_2: " + str(line_2))
+    print ("x: " + str(x) + ", y: " + str(y))
+
+    small_x = [x]
+    small_x.append(min(line_1.src.x, line_1.dst.x))
+    small_x.append(min(line_2.src.x, line_2.dst.x))
+
+    large_x = [x]
+    large_x.append(max(line_1.src.x, line_1.dst.x))
+    large_x.append(max(line_2.src.x, line_2.dst.x))
+
+    small_y = [y]
+    small_y.append(min(line_1.src.y, line_1.dst.y))
+    small_y.append(min(line_2.src.y, line_2.dst.y))
+
+    large_y = [y]
+    large_y.append(max(line_1.src.y, line_1.dst.y))
+    large_y.append(max(line_2.src.y, line_2.dst.y))
+
+    if max(small_x) == x and min(large_x) == x and max(small_y) == y and min(large_y) == y:
+        drawPoint(Vector2(x, y))
+
+def drawPoint(vector2):
+    print(str(vector2))
+    point = worldToScreenPoint(vector2)
+    glPointSize(10.0)
+    glColor3f(1.0, 1.0, 1.0)
+    glBegin(GL_POINTS)
+    glVertex2f(point.x, point.y)
+    glEnd()
+
+def drawLine(line):
+    origin = worldToScreenPoint(line.src)
+    destination = worldToScreenPoint(line.dst)
+    
     glLineWidth(2.5)
     glColor3f(1.0, 0.0, 0.0)
     glBegin(GL_LINES)
